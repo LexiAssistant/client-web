e7eab03095fc3a64232be4684e66166692033c09
Suhwan0818 <tnghks1997@naver.com>
2024-06-16 14:48:45 +0900

    💄 tailwind CSS 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div>
-      <h1>문서 자동 처리 시스템</h1>
+      <h1 className="text-4xl font-bold text-blue-500">
+        문서 자동 처리 시스템
+      </h1>
     </div>
   );
 };
